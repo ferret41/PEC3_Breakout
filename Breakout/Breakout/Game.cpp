c5c6230e727c9b2352e03d3fd0f192ff29850b8b
ferret41 <martiferret41@gmail.com>
2024-01-14 12:41:55 +0100

    rendering sprites

--- Breakout/Breakout/Game.cpp
+++ Breakout/Breakout/Game.cpp
@@ -7,6 +7,13 @@
 ** option) any later version.
 ******************************************************************/
 #include "Game.h"
+#include "ResourceManager.h"
+#include "SpriteRenderer.h"
+
+
+// Game-related State data
+SpriteRenderer* Renderer;
+
 
 Game::Game(unsigned int width, unsigned int height)
     : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -16,12 +23,23 @@ Game::Game(unsigned int width, unsigned int height)
 
 Game::~Game()
 {
-
+    delete Renderer;
 }
 
 void Game::Init()
 {
-
+    // load shaders
+    ResourceManager::LoadShader("shaders/sprite.vs", "shaders/sprite.frag", nullptr, "sprite");
+    // configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // set render-specific controls
+    Shader shader_sprite = ResourceManager::GetShader("sprite");
+    Renderer = new SpriteRenderer(shader_sprite);
+    // load textures
+    ResourceManager::LoadTexture("textures/awesomeface.png", true, "face");
 }
 
 void Game::Update(float dt)
@@ -36,5 +54,6 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
-
+    Texture2D face_texture = ResourceManager::GetTexture("face");
+    Renderer->DrawSprite(face_texture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
